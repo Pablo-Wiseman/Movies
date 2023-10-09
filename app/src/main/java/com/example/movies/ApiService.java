f02e390bffdb70119aa16ee77f0f011109711f1f
Pablo-Wiseman <accessdevelopment@yandex.ru>
2023-10-09 18:44:44 +0500

    api-service realized

---
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.movies;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    public static String apiKey = BuildConfig.API_KEY;
+
+    @GET("movie?token=apiKey&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=40")
+    Single<MovieResponse> loadMovies();
+}
