5e59f444905b6ef527620aa30067d0ff56e65ae7
Pablo-Wiseman <accessdevelopment@yandex.ru>
2023-10-10 16:35:15 +0500

    background shape realized & data loading

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -8,6 +8,6 @@ public interface ApiService {
 
     public static String apiKey = BuildConfig.API_KEY;
 
-    @GET("movie?token="+apiKey+"&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=40")
+    @GET("movie?token="+apiKey+"&field=rating.kp&search=4-8&sortField=votes.kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 }
